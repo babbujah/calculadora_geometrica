fec0530d177be48d079b125a494c37727e7432d3
bczion <bczion@hotmail.com>
2018-04-21 21:01:54 -0300

    Adição da classe Cubo

---
+++ cubo.cpp
@@ -0,0 +1,33 @@
+#include "cubo.h"
+
+Cubo::Cubo( Retangulo* face ): m_face( face ), m_area( 0 ), m_volume( 0 ){}
+
+Cubo::Cubo( Cubo &cubo ){
+    m_face = cubo.getFace();
+    m_area = cubo.getArea();
+    m_volume = cubo.getVolume();
+}
+
+Cubo::~Cubo(){}
+
+std::ostream& operator<< ( std::ostream &o, Cubo const c ){
+    o << std::endl << "##### CUBO #####" << std::endl <<
+    "-> Área dos Lados: " << c.m_face->getArea() << UNID << std::endl <<
+    "-> Área: " << c.m_area << UNID << std::endl <<
+    "-> Volume: " << c.m_volume << UNID << std::endl;
+    return o;
+}
+
+Retangulo* Cubo::getFace(){
+    return m_face;
+}
+
+double Cubo::getArea(){
+    m_area = 6 * m_face->getArea();
+    return m_area;
+}
+
+double Cubo::getVolume(){
+    m_volume = pow(m_face->getArea(), 3);
+    return m_volume;
+}
---
+++ cubo.h
@@ -0,0 +1,28 @@
+#ifndef CUBO_H
+#define CUBO_H
+
+#include <cmath>
+
+#include "retangulo.h"
+
+#define UNID " u"
+
+class Cubo{
+    private:
+        Retangulo * m_face;
+        double m_area;
+        double m_volume;
+
+    public:
+        Cubo( Retangulo* );
+        Cubo( Cubo &cubo );
+        ~Cubo();
+
+        friend std::ostream& operator<< ( std::ostream &o, Cubo const c );
+
+        Retangulo* getFace();
+        double getArea();
+        double getVolume();
+};
+
+#endif
